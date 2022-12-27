435dba1e43a942aab13c391530405c09e7fefa59
satya bhusan prusty <satyabhusan9853@gmail.com>
2022-12-27 23:24:18 +0530

    Created Login Page with form Validation using formik and Yup packages, added Dynamic css Styles for page responsiveness

---
+++ frontend/src/components/shared/inputs/login-input/Login-input.js
@@ -0,0 +1,43 @@
+import { ErrorMessage, useField } from "formik";
+import { useMediaQuery } from "react-responsive";
+
+import Style from "./Login-input.module.css";
+
+const LoginInput = (props) => {
+  const [field, meta] = useField(props);
+
+  // setting desktopVIew which will help us to get the display size
+  const desktopView = useMediaQuery({ query: "(min-width:850px)" }); // sends true if display size is more than 850px and false otherwise
+
+  const errorMessageDiv = (
+    <div
+      className={desktopView ? `${Style["input_error"]} ${Style["input_error_desktop"]}` : Style["input_error"]}
+      style={{ transform: "translateY(3px)" }}
+    >
+      <ErrorMessage name={field.name} />
+      <div
+        className={
+          props.bottom
+            ? desktopView
+              ? Style["error_arrow_right"]
+              : Style["error_arrow_bottom"]
+            : desktopView
+            ? Style["error_arrow_right"]
+            : Style["error_arrow_top"]
+        }
+      ></div>
+    </div>
+  );
+
+  return (
+    <div className={Style["input_wrap"]}>
+      {meta.touched && meta.error && !props.bottom && errorMessageDiv}
+      <input className={meta.touched && meta.error ? Style["input_error_border"] : ""} {...field} {...props} />
+      {meta.touched && meta.error && props.bottom && errorMessageDiv}
+
+      {meta.touched && meta.error && <i className="error_icon" style={{ top: `${!props.bottom && !desktopView ? "63%" : "15px"}` }}></i>}
+    </div>
+  );
+};
+
+export default LoginInput;
--- frontend/src/pages/login/User-login.js
+++ frontend/src/pages/login/User-login.js
@@ -1,5 +1,73 @@
+import { useState } from "react";
+import { Formik, Form } from "formik";
+import { Link } from "react-router-dom";
+import * as Yup from "yup";
+
+import LoginInput from "../../components/shared/inputs/login-input/Login-input";
+import Style from "./User-login.module.css";
+
 const UserLogin = () => {
-  return <div>Login</div>;
+  // managing states
+  const [login, setLogin] = useState({ email: "", password: "" });
+
+  // handle inputs after submit
+  const handleLogin = (event) => {
+    const { name, value } = event.target;
+
+    setLogin((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  // setting login Validaiton Schema using Yup
+  const loginValidation = Yup.object({
+    email: Yup.string().required("Email is required").email("Please enter a valid email").max(100, "Maximum character reached"),
+    password: Yup.string().required("Password is required").min(6, "Password length should be atleast 6 characters"),
+  });
+
+  // setting formik component
+  const formik = (formik) => {
+    return (
+      <Form>
+        <LoginInput type="text" name="email" placeholder="Email address or Phone Number" onChange={handleLogin} />
+        <LoginInput type="password" name="password" placeholder="Password" onChange={handleLogin} bottom={+true} />
+        <button type="submit" className="blue_btn">
+          Log In
+        </button>
+      </Form>
+    );
+  };
+
+  return (
+    <div className={Style["login"]}>
+      <div className={Style["login_wrapper"]}>
+        <div className={Style["login_wrap"]}>
+          <div className={Style["login_1"]}>
+            {/* Change this Image with ShareBook SVG */}
+            <img src="../../icons/facebook.svg" alt="" />
+            <span>Sharebook helps you connect and share with the people in your life</span>
+          </div>
+          <div className={Style["login_2"]}>
+            <div className={Style["login_2_wrap"]}>
+              <Formik enableReinitialize initialValues={{ email: login.email, password: login.password }} validationSchema={loginValidation}>
+                {formik}
+              </Formik>
+              <Link to="/forgot" className={Style["forgot_password"]}>
+                Forgotten Password?
+              </Link>
+              <div className={Style["sign_splitter"]}></div>
+              <button className={`${"blue_btn"} ${Style["open_signup"]}`}>Create Account</button>
+            </div>
+            <Link to="/" className={Style["sign_extra"]}>
+              <b>Create a page </b>
+              for a celebrity, brand or business
+            </Link>
+          </div>
+        </div>
+        <div className={Style["register"]}></div>
+      </div>
+    </div>
+  );
 };
 
 export default UserLogin;
